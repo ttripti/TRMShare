62ec452c50061bf2cb4537015a94e1ddaf207838
TT <103045884+ttripti@users.noreply.github.com>
2023-04-28 10:26:05 -0500

    Add files via upload

---
+++ ReportORDGlobalTag.aspx
@@ -0,0 +1,155 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="ReportORDGlobalTag.aspx.vb" Inherits="VA.TRM.PresentationO.ReportORDGlobalTag" %>
+<%@ Register Src="~/UserControls/ucToolLinkComponent.ascx" TagName="ucToolLinkComponent" TagPrefix="uc" %>
+
+<%@ Import Namespace="System.Text.RegularExpressions" %>
+<%@ Import Namespace="System.Data.SqlClient" %>
+<%@ Import Namespace="VA.TRM.PresentationO.TRMPresentation.inc.TRMConfig.TRMConfig" %>
+<%@ Import Namespace="VA.TRM.PresentationO.TRMPresentation.inc.TRMCodeLib" %>
+<%@ Import Namespace="VA.TRM.PresentationO.TRMPresentation.inc.TRMGlobals" %>
+
+<asp:Content ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+   <style type="text/css">
+        .RowStyle {
+          height: 100px;
+        }
+         .GridViewStandard {
+            width:100% !important;
+        }
+
+        .GridViewStandard th{
+            font-family: arial;
+	        font-size: 95%;
+	        font-weight: bold;
+	        text-align: center;
+	        padding-left: 5px;
+	        padding-right: 5px;
+	        padding-top: 1px;
+	        padding-bottom: 1px;
+	        color: black;
+	        background-color:#BBBFFF;
+	        border: 1px #999999 solid;
+            white-space: nowrap;
+        }
+
+         .GridViewStandard td{
+            font-family: arial;
+	        font-size: 90%;
+	        background-color: #E4E4E4;
+	        color: #4A452A;
+	        border: 1pt  #d9d9d9 solid;
+	        text-align: left;
+	        white-space:normal;
+	        padding-top: 1px;
+	        padding-bottom: 1px;
+	        padding-left: 5px;
+	        padding-right: 5px;
+            text-wrap:normal;
+            height : 30%;
+        } 
+
+        .TableCellCenter{
+	        
+	        text-align:center !important;  
+	        padding-left: 5px;
+	        padding-right: 5px;
+        }
+
+        .TableLargeText{
+            width:70%;
+            word-wrap:break-word;
+        }
+
+    </style>
+      <script type="text/javascript">
+        $(function () {
+            $('[id*=submitSpinner]').hide();
+            $('[id*=submitSpinner]').css("display", "none");
+            $('[id*=btnDecisionType]').on("click", function () {
+                setTimeout(function () {
+                    $('[id*=btnDecisionType]').attr("diabled","disabled");
+                    $('[id*=submitSpinner]').show();
+                    $('[id*=submitSpinner]').css("display","inline-block");
+                }, 200);
+            });
+        });
+      </script>
+    <%
+        trmCodeLib.displayPageTitle("ORD Global Tags", "")
+     %>
+        <br />
+        The following table displays ORD global tags tagged to non-archived entries within the VA TRM. The ORD global tags are mananged by the Office of Research and Development.
+        <br />
+        <br />
+        <div style="width:100%; overflow: auto; overflow-y: hidden; ">
+
+            <table>
+                <tr>
+                    <td scope="row" width="150px" style="font-weight: bold; color: blue; font-style: italic; text-align: left">Select a decision type:</td>
+                    <td width="165px">
+                        <asp:DropDownList runat="server" ID="ddlDecisionType">
+                        <asp:ListItem Text="All" Value="All"> </asp:ListItem>
+                        <asp:ListItem Text="Approved" Value="Approved"> </asp:ListItem>
+                        <asp:ListItem Text="Approved w/Constraints" Value="Approved w/Constraints"> </asp:ListItem>
+                        <asp:ListItem Text="Divest" Value="Divest"> </asp:ListItem>
+                        <asp:ListItem Text="Prohibited" Value="Prohibited"> </asp:ListItem>
+                        <asp:ListItem Text="Unapproved" Value="Unapproved"> </asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                    <td width="100px">                
+                        <asp:Button runat="server" ID="btnDecisionType" OnClick="btnDecisionType_Click" Text="Submit" ToolTip="Button to generate the report"/>
+                        <div style="display: none; width:fit-content;vertical-align: middle;" id="submitSpinner">
+                                <i class="fa-solid fa-circle-notch fa-spin"></i>
+                     </div>
+                   </td>
+                   <td style="padding-left:100px">
+	               
+                           <div runat="server" id="exportDiv" visible="false">
+                           Export To:
+                           <asp:ImageButton runat="server" ID="imgExcel" AlternateText="Generate Excel File" 
+                               Width="32" Height="32" OnClick="imgExcel_Click" ImageUrl="~/images/excel.png" />
+                           <asp:ImageButton runat="server" ID="imgWord" AlternateText="Generate Word File" 
+                               Width="32" Height="32" ImageUrl="~/images/word.png" OnClick="imgWord_Click"/>
+                           </div>
+			        
+                   </td>
+                </tr>
+                <tr>
+                    <td colspan="4">
+                        <center><b>List of Entries Mapped to ORD Global Tags</b></center>
+                    </td>
+                </tr>      
+                <tr>
+                    <td colspan="4">
+                    <%
+                        If (True) Then
+                    %>
+                            <asp:GridView runat="server" ID="gvDecisionType" AutoGenerateColumns="false" EnableViewState="false"
+                                OnSorting="gvDecisionType_Sorting" OnSorted="gvDecisionType_Sorted"  AllowPaging="false" 
+                                OnRowDataBound="gvDecisionType_RowDataBound" GridLines="Horizontal" AllowSorting="true" ShowFooter="true"
+                                OnPageIndexChanging="gvDecisionType_PageIndexChanging" RowStyle-CssClass="RowStyle" CssClass="GridViewStandard"  OnDataBound="gvDecisionType_DataBound">
+                    
+                                <Columns>
+                                    
+                                    <asp:BoundField SortExpression="tagName" HeaderText="Tag Name" DataField="tagName" ItemStyle-Width="15%" ItemStyle-CssClass="TableCellCenter"/>
+                                    <asp:BoundField SortExpression="tagDefinition" HeaderText="Tag Definition" DataField="tagDefinition" ItemStyle-Width="40%" ItemStyle-CssClass="TableCellCenter"/>
+                                    <asp:TemplateField SortExpression="toolName" HeaderText="Software" ItemStyle-Width="12%">
+                                        <ItemStyle/> 
+                                        <ItemTemplate>
+                                           <uc:ucToolLinkComponent runat="server" ID="ucToolLinkComponent" />                   
+                                        </ItemTemplate>
+                                    </asp:TemplateField>
+                                    <asp:BoundField HeaderText="Version" DataField="toolVersionNo" SortExpression="toolVersionNo" ItemStyle-Width="8%"/>
+                                    <asp:BoundField HeaderText="Decision" DataField="toolDecision" SortExpression="toolDecision" ItemStyle-Width="13%"/>
+						            <asp:BoundField HeaderText="Date Tagged" SortExpression="tagDate" DataField="tagDate" ItemStyle-Width="20%" ItemStyle-CssClass="TableCellCenter" DataFormatString="{0:G}" />
+                                </Columns>                   
+                            </asp:GridView>
+                    <%
+                        End If
+                    %>
+                    </td>
+                </tr>       
+            </table>
+        </div>
+   
+    
+</asp:Content>
---
+++ ReportORDGlobalTag.aspx.vb
@@ -0,0 +1,327 @@
+﻿Imports VA.TRM.Domain
+Imports System.String
+Imports VA.TRM.BusinessService
+Imports System.IO
+Imports VA.TRM.Domain.CustomEntities
+Imports VA.TRM.PresentationO.TRMPresentation.inc.TRMConfig.TRMConfig
+Imports VA.TRM.BusinessService.Excel
+Imports System.Drawing
+Imports VA.TRM.PresentationO.TRMPresentation.inc.TRMRequestCode.TRMRequestCode
+'TODO: We need to import this file for one global variable... 
+Imports VA.TRM.PresentationO.TRMPresentation.inc.TRMRequestCode
+Imports Newtonsoft.Json
+'Imports Microsoft.Office.Interop.Excel
+
+Imports VA.TRM.PresentationO.TRMPresentation.inc.TRMGlobals
+Imports System.Web.Services
+Imports System.Web.Script.Services
+
+Public Class ReportORDGlobalTag : Inherits BasePage
+
+    Private reportGlobalTagService_ As Abstract.IGlobalTagReportService
+    Const wgId = 62
+    Protected ReadOnly Property ReportGlobalTagService() As Abstract.IGlobalTagReportService
+        Get
+            If reportGlobalTagService_ Is Nothing Then
+                reportGlobalTagService_ = New Concrete.GlobalTagReportService(ConnectionString)
+            End If
+            Return reportGlobalTagService_
+        End Get
+    End Property
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If (Not IsPostBack) Then
+            ddlDecisionType.SelectedValue = Request.Params("decisionType")
+            LoadReportData()
+            gvDecisionType.DataBind()
+            exportDiv.Visible = True
+        End If
+    End Sub
+    Protected Sub gvDecisionType_DataBound(sender As Object, e As EventArgs)
+
+
+    End Sub
+    Protected Sub LoadReportData()
+        Try
+            Dim globalTagReport As List(Of GlobalTagReport) = ReportGlobalTagService.GetGlobalTagReport(ddlDecisionType.SelectedValue, wgId, SortColumn, SortOrder)
+            For Each rdt As GlobalTagReport In globalTagReport
+                If (rdt.tagDefinition = "") Then
+                    _showDefinition = False
+                Else
+                    _showDefinition = True
+                End If
+            Next
+            gvDecisionType.DataSource = globalTagReport
+            exportDiv.Visible = True
+
+        Catch ex As Exception
+            System.Diagnostics.Debug.WriteLine(ex.Message)
+        End Try
+    End Sub
+
+    Protected Sub btnDecisionType_Click(sender As Object, e As EventArgs)
+        ' System.Threading.Thread.Sleep(1000)
+        SortColumn = ""
+        SortOrder = GlobalEnums.SortOrder.Ascending
+        LoadReportData()
+        gvDecisionType.DataBind()
+        exportDiv.Visible = True
+    End Sub
+
+#Region "Properties"
+    Private _showDefinition As Boolean
+
+    Public Property ShowDefinition() As Boolean
+        Get
+            Return _showDefinition
+        End Get
+        Set(ByVal value As Boolean)
+            _showDefinition = value
+        End Set
+    End Property
+    Protected Property SortColumn As String
+        Get
+            If IsNothing(Session("RepSortColumn")) Then
+                Return ""
+            End If
+            Return Session("RepSortColumn")
+        End Get
+        Set(value As String)
+            If IsNothing(value) Then
+                value = "tagName"
+            End If
+            Session("RepSortColumn") = value
+        End Set
+    End Property
+
+    Public Property SortOrder As GlobalEnums.SortOrder
+        Get
+            If IsNothing(Session("RepSortOrder")) Then
+                Session("RepSortOrder") = GlobalEnums.SortOrder.Ascending
+                Return Session("RepSortOrder")
+            End If
+            Return Session("RepSortOrder")
+        End Get
+        Set(value As GlobalEnums.SortOrder)
+            Session("RepSortOrder") = value
+        End Set
+    End Property
+
+#End Region
+
+    Protected Sub imgExcel_Click(sender As Object, e As ImageClickEventArgs)
+        Try
+            Dim title As String = "TRM_" & ddlDecisionType.SelectedValue.ToString() & "_ORD_Global_Tag_Report"
+            Dim fileName As String = Utilities.FileAppendDate(title) & ".xlsx"
+            Response.Clear()
+            Response.Buffer = True
+            Response.AddHeader("content-disposition", "attachment;filename=" & fileName)
+            Response.Charset = ""
+            Response.ContentType = "application/vnd.ms-excel"
+
+            Dim resultList As List(Of GlobalTagReport) = ReportGlobalTagService.GetGlobalTagReport(ddlDecisionType.SelectedValue, wgId, SortColumn, SortOrder)
+            For Each rdt As GlobalTagReport In resultList
+                If (rdt.tagDefinition = "") Then
+                    _showDefinition = False
+                Else
+                    _showDefinition = True
+                End If
+            Next
+            Dim colView As Excel.ExcelColumnView = New Excel.ExcelColumnView
+            colView.worksheetTitle = title
+
+            colView.headerText = String.Format("The following table displays ORD global tags tagged to non-archived entries within the VA TRM. The ORD global tags are mananged by the Office of Research and Development.")
+            'colView.footerText = String.Format("{0} Entries reporting {1} {2} Versions", ToolCount.ToString(), EntryCount.ToString(), ddlDecisionType.SelectedValue.ToString())
+
+            colView.dataStartRow = 4
+            colView.columns.Add(New ExcelColumn("Tag Name", "tagName", True, "", False, True))
+
+            If _showDefinition Then
+                colView.columns.Add(New ExcelColumn("Tag Definition", "tagDefinition", True, "", False, True))
+            End If
+            colView.columns.Add(New ExcelColumn("Software", "toolName", True, "", False, True))
+            colView.columns.Add(New ExcelColumn("Version", "toolVersionNo", True, "", False, True))
+            colView.columns.Add(New ExcelColumn("Decision", "toolDecision", True, "", False, True))
+            colView.columns.Add(New ExcelColumn("Decision Date", "tagDate", True, "", False, True))
+
+            Dim excelDocument As Excel.ExcelDocument = New Excel.ExcelDocument(colView)
+            Response.BinaryWrite(excelDocument.generateExcelDoc(resultList).GetAsByteArray())
+            Response.Flush()
+            Response.End()
+        Catch abortEx As System.Threading.ThreadAbortException
+            'Do nothing
+        Catch ex As Exception
+            Response.Write("Encountered following Error Error in imgExcel_Click:")
+            Response.Write(System.Environment.NewLine)
+            Response.Write(ex)
+        Finally
+            HttpContext.Current.Response.SuppressContent = True
+            HttpContext.Current.ApplicationInstance.CompleteRequest()
+        End Try
+    End Sub
+    Protected Function GenerateHeaders() As String
+        Dim sb As StringBuilder = New StringBuilder()
+        sb.Append("<html>")
+        sb.Append("<head>")
+        sb.Append("</head>")
+        sb.Append("<body>")
+        sb.Append("<table>")
+        sb.Append("<tr>")
+        sb.Append("<td colspan=""6"">")
+        sb.Append(String.Format("The following table lists published standards and technologies that have a decision of: {0} as of {1}", ddlDecisionType.SelectedValue, Today.ToShortDateString()))
+        sb.Append("</td>")
+        sb.Append("<td>")
+        sb.Append("</tr>")
+        sb.Append("<tr>")
+        sb.Append("<td colspan=""6"">&nbsp;</td>")
+        sb.Append("</tr>")
+        sb.Append("</table>")
+
+        sb.Append("<table>")
+        sb.Append("<tr>")
+        sb.Append("<th style=""width:5%""  scope=""col"">")
+        sb.Append("Tag Name")
+        sb.Append("</th>")
+        If _showDefinition Then
+            sb.Append("<th style=""width:15%""  scope=""col"">")
+            sb.Append("Tag Definition")
+            sb.Append("</th>")
+        End If
+        sb.Append("<th style=""width:5%""  scope=""col"">")
+        sb.Append("Software")
+        sb.Append("</th>")
+
+        sb.Append("<th style=""width:5%"" scope=""col"">")
+        sb.Append("Version")
+        sb.Append("</th>")
+
+        sb.Append("<th style=""width:10%"" scope=""col"">")
+        sb.Append("Decision")
+        sb.Append("</th>")
+
+        sb.Append("<th style=""width:20%"" scope=""col"">")
+        sb.Append("Date Tagged")
+        sb.Append("</th>")
+        sb.Append("</tr>")
+
+        Return sb.ToString()
+    End Function
+
+    Protected Function GenerateFooter() As String
+        Dim sb As StringBuilder = New StringBuilder()
+        sb.Append("<table>")
+        sb.Append("<tr>")
+        sb.Append("<td colspan=""6"">&nbsp;</td>")
+        sb.Append("</tr>")
+        sb.Append("<tr>")
+        sb.Append("</tr>")
+        sb.Append("</table>")
+        sb.Append("</body>")
+        sb.Append("</html>")
+        Return sb.ToString()
+    End Function
+    Protected Sub imgWord_Click(sender As Object, e As ImageClickEventArgs)
+        Try
+            Dim fileName As String = "TRM_" & ddlDecisionType.SelectedValue.ToString() & "_ORD_Global_Tag_Report"
+            fileName = Utilities.FileAppendDate(fileName) & ".doc"
+            Response.Clear()
+            Response.Buffer = True
+            Response.AddHeader("content-disposition", "attachment;filename=" & fileName)
+            Response.Charset = ""
+            Response.ContentType = "application/vnd.msword"
+
+            Dim resultList As List(Of GlobalTagReport) = ReportGlobalTagService.GetGlobalTagReport(ddlDecisionType.SelectedValue, wgId, SortColumn, SortOrder)
+            For Each rdt As GlobalTagReport In resultList
+                If (rdt.tagDefinition = "") Then
+                    _showDefinition = False
+                Else
+                    _showDefinition = True
+                End If
+            Next
+            Dim results As String = resultList.ConvertListToStringInTableFormat(getColumnDictionary())
+            Dim pos As Integer = results.IndexOf("<table>")
+
+            If pos = 0 Then
+                results = results.Substring(pos + "<table>".Length, results.Length - pos - "<table>".Length)
+            End If
+
+            Response.Output.Write(GenerateHeaders())
+            Response.Output.Write(results)
+            Response.Output.Write(GenerateFooter())
+            Response.Flush()
+            Response.End()
+        Catch abortEx As System.Threading.ThreadAbortException
+            'Do nothing
+        Catch ex As Exception
+            Response.Write("Encountered following Error Error in imgWord_Click:")
+            Response.Write(System.Environment.NewLine)
+            Response.Write(ex)
+        Finally
+            HttpContext.Current.Response.SuppressContent = True
+            HttpContext.Current.ApplicationInstance.CompleteRequest()
+        End Try
+    End Sub
+
+
+    Private Sub excelFormatToolLink(evtData As CellChangeData)
+        Dim cellData As GlobalTagReport = DirectCast(evtData.cellData, GlobalTagReport)
+    End Sub
+
+    Protected Function getColumnDictionary() As Dictionary(Of Integer, String)
+        Dim dic As Dictionary(Of Int32, String) = New Dictionary(Of Integer, String)()
+        dic.Add(0, "tagName")
+        dic.Add(1, "tagDefinition")
+        dic.Add(2, "toolName")
+        dic.Add(3, "toolVersionNo")
+        dic.Add(4, "toolDecision")
+        dic.Add(5, "tagDate")
+        Return dic
+    End Function
+
+    Protected Sub gvDecisionType_Sorted(sender As Object, e As EventArgs)
+        gvDecisionType.DataBind()
+    End Sub
+
+    Protected Sub gvDecisionType_Sorting(sender As Object, e As GridViewSortEventArgs)
+
+        If (SortColumn = e.SortExpression Or String.IsNullOrEmpty(SortColumn)) Then
+            If (SortOrder = GlobalEnums.SortOrder.Ascending) Then
+                SortOrder = GlobalEnums.SortOrder.Descending
+            Else
+                SortOrder = GlobalEnums.SortOrder.Ascending
+            End If
+            'SortColumn = e.SortExpression
+        Else
+            SortOrder = GlobalEnums.SortOrder.Ascending
+        End If
+
+        SortColumn = e.SortExpression
+        LoadReportData()
+
+    End Sub
+
+    Protected Sub gvDecisionType_RowDataBound(sender As Object, e As GridViewRowEventArgs)
+        If _showDefinition Then
+            e.Row.Cells(1).Visible = True
+        Else
+            e.Row.Cells(1).Visible = False
+        End If
+
+        If (e.Row.RowType = DataControlRowType.DataRow) Then
+
+            Dim tlc As ucToolLinkComponent = DirectCast(e.Row.FindControl("ucToolLinkComponent"), ucToolLinkComponent)
+            tlc.Tool = DirectCast(e.Row.DataItem, GlobalTagReport).PresentationTool
+            tlc.loadControls()
+
+        End If
+
+    End Sub
+
+
+    Protected Sub gvDecisionType_PageIndexChanging(sender As Object, e As GridViewPageEventArgs)
+        LoadReportData()
+        gvDecisionType.PageIndex = e.NewPageIndex
+        gvDecisionType.DataBind()
+    End Sub
+
+End Class
